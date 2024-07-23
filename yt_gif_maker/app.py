42967b76a1091830b81b48c849252dd84dc11d42
Jeremy Watt <jermwatt@gmail.com>
2024-07-23 17:15:29 -0500

    streamlit app framework moved over

--- yt_gif_maker/app.py
+++ yt_gif_maker/app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 from yt_gif_maker.transcribe import avaliable_models
 from yt_gif_maker.transcribe import transcribe
-from yt_gif_maker.audio_extractor import extract_audio
-from yt_gif_maker.create import bleep_replace
 from yt_gif_maker.yt_download import download_video
+from yt_gif_maker.nearest import get_nearest_snippets
+from yt_gif_maker.gif_maker import clip_video_and_gif, draw_on_gif
+import base64
 import tempfile
 import uuid
 import io
@@ -31,22 +32,22 @@ with tab2:
 with tab1:
     with st.container(border=True):
         upload_url = st.text_input(
-            label="youtube url",
-            value="https://www.youtube.com/watch?v=9m_12SGXNKw",
+            label="YouTube / Shorts url",
+            value="https://www.youtube.com/shorts/43BhDHYBG0o",
         )
 
     with st.container(border=True):
         col1, col2, col3 = st.columns([8, 3, 4])
         with col1:
-            bleep_words = st.text_area(
+            input_phrase = st.text_area(
                 label="input phrase",
                 placeholder="enter in the input phrase you'd like gif-a-fied",
-                value="say hello to my little friend",
+                value="every time we'd mention costa rica",
             )
         with col2:
             model_selection = st.selectbox(
                 label="whisper model (base only in HF space)",
-                index=1,
+                index=0,
                 options=avaliable_models,
             )
         with col3:
@@ -67,9 +68,10 @@ with tab1:
     def button_logic(
         temporary_video_location: str,
         model_selection: str,
-        phrase: list,
+        input_phrase: list,
         upload_url: str,
     ):
+        col_empty_1, col_orig_video, col_empty_2 = st.columns([4, 8, 4])
 
         if trans_button_val:
             download_video(upload_url, temporary_video_location)
@@ -77,11 +79,11 @@ with tab1:
             byte_file = io.BytesIO(filename.read())
             with open(temporary_video_location, "wb") as out:
                 out.write(byte_file.read())
-                with st.container(border=True):
-                    with colo1:
+                with col_orig_video:
+                    with st.container(border=True):
                         st.caption("original video")
                         st.video(temporary_video_location)
-                out.close()
+                    out.close()
 
             transcript, timestamped_words  = transcribe(video_file_path=temporary_video_location, model=model_selection)
 
@@ -98,13 +100,20 @@ with tab1:
             byte_file = io.BytesIO(filename.read())
             with open(temporary_video_location, "wb") as out:
                 out.write(byte_file.read())
-                with st.container(border=True):
-                    with colo1:
+                with col_orig_video:
+                    with st.container(border=True):
                         st.caption("original video")
                         st.video(temporary_video_location)
-                out.close()
+                    out.close()
 
             transcript, timestamped_words  = transcribe(video_file_path=temporary_video_location, model=model_selection)
+            closest_time_ranges = get_nearest_snippets(input_phrase, transcript, timestamped_words) 
+            
+            output_clip_path = temporary_video_location[:-4] + "_clip_1.mp4"
+            output_gif_path = temporary_video_location[:-4] + "_clip_1.gif"
+            start_ms = closest_time_ranges[0][0]
+            end_ms = closest_time_ranges[0][1]
+            clip_video_and_gif(temporary_video_location, output_clip_path, output_gif_path, start_ms, end_ms)
 
             with col0.container(border=True):
                 st.text_area(
@@ -112,11 +121,50 @@ with tab1:
                     placeholder="transcribe text will be shown here",
                     label="transcribe text",
                 )
-
+                
+            c00, c01, c02 = st.columns([8, 8, 8])
+            filename = open(output_clip_path, "rb")
+            byte_file = io.BytesIO(filename.read())
+            with open(output_clip_path, "wb") as out:
+                out.write(byte_file.read())
+                with c00:
+                    with st.container(border=True):
+                        st.caption("clip 1 video")
+                        st.video(output_clip_path)
+                    out.close()
+                    
+                       
+            filename = open(output_gif_path, "rb")
+            byte_file = io.BytesIO(filename.read())
+            with open(temporary_video_location, "wb") as out:
+                out.write(byte_file.read())
+                with c01:
+                    with st.container(border=True):
+                        st.caption("clip 1 gif")
+                        st.image(output_gif_path)
+                    out.close()
+                    
+                    with open(output_gif_path, "rb") as file:
+                        st.download_button(
+                            label="Download gif",
+                            data=file,
+                            file_name="clip_1.gif",
+                            mime="image/gif"
+                        )
+                        
+                        # file_ = open(output_gif_path, "rb")
+                        # contents = file_.read()
+                        # data_url = base64.b64encode(contents).decode("utf-8")
+                        # file_.close()
+                        # st.markdown(
+                        #     f'<img src="data:image/gif;base64,{data_url}" alt="clip 1 gif">',
+                        #     unsafe_allow_html=True,
+                        # )
+    
+                    out.close()
+                    
 
 
     with tempfile.TemporaryDirectory() as tmpdirname:
         temporary_video_location = tmpdirname + "/original_" + str(uuid.uuid4()) + ".mp4"
-        bleep_word_list = bleep_words.split(",")
-        bleep_words_list = [v.strip() for v in bleep_word_list if len(v.strip()) > 0]
-        button_logic(temporary_video_location, model_selection, bleep_words_list, upload_url)
+        button_logic(temporary_video_location, model_selection, input_phrase, upload_url)
