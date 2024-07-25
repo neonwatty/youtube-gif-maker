56de68ec413f152269d061f364139af73137675b
Jeremy Watt <jermwatt@gmail.com>
2024-07-25 14:41:15 -0700

    session state for whisper transcript added

--- yt_gif_maker/app.py
+++ yt_gif_maker/app.py
@@ -21,7 +21,9 @@ if "whisper_transcript_words" not in st.session_state:
 if "whisper_just_transcript" not in st.session_state:
     st.session_state.whisper_just_transcript = ""
 if "temporary_video_location" not in st.session_state:
-    st.session_state.temporary_video_location = ""
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        st.session_state.temporary_video_location = tmpdirname + "/original_" + str(uuid.uuid4()) + ".mp4"
+    
 if "upload_url" not in st.session_state:
     st.session_state.upload_url = "https://www.youtube.com/shorts/43BhDHYBG0o"
 if "input_phrase" not in st.session_state:
@@ -29,33 +31,46 @@ if "input_phrase" not in st.session_state:
         st.session_state.input_phrase = "every time we'd mention costa rica"
     else:
         st.session_state.input_phrase = ""
+if "model_selection" not in st.session_state:
+    st.session_state.model_selection = "base"
+if "model_selection_index" not in st.session_state:
+    st.session_state.model_selection_index = 1
 
 
 def fetch_logic(upload_url: str, temporary_video_location: str):
-    if yt_fetch_button:
-        download_video(upload_url, temporary_video_location)
-        filename = open(temporary_video_location, "rb")
-        byte_file = io.BytesIO(filename.read())
-        with open(temporary_video_location, "wb") as out:
-            out.write(byte_file.read())
-            with col_orig_video:
-                with st.container(border=True):
-                    st.caption("original video")
-                    st.video(temporary_video_location)
-                out.close()
-        yt_transcript = get_single_transcript(upload_url)
+    download_video(upload_url, temporary_video_location)
+    filename = open(temporary_video_location, "rb")
+    byte_file = io.BytesIO(filename.read())
+    with open(temporary_video_location, "wb") as out:
+        out.write(byte_file.read())
+        with col_orig_video:
+            with st.container(border=True):
+                st.caption("original video")
+                st.video(temporary_video_location)
+            out.close()
+    yt_transcript = get_single_transcript(upload_url)
 
-        st.session_state.yt_transcript_text = yt_transcript
+    st.session_state.yt_transcript_text = yt_transcript
 
-        all_text = yt_transcript["transcript"]
-        all_text = " ".join([v["text"] for v in all_text])
+    all_text = yt_transcript["transcript"]
+    all_text = " ".join([v["text"] for v in all_text])
 
-        st.session_state.yt_just_transcript_text = all_text
+    st.session_state.yt_just_transcript_text = all_text
+
+
+def transcribe_logic(
+    temporary_video_location: str,
+    model_selection: str,
+):
+    st.session_state.whisper_just_transcript, st.session_state.whisper_transcript_words = transcribe(video_file_path=temporary_video_location, model=model_selection)
 
 
 app_name = "YouTube gif maker (advanced)"
 st.set_page_config(page_title=app_name)
 st.title(app_name)
+
+
+
 
 tab1, tab2 = st.tabs([app_name, "💡 About"])
 
@@ -72,7 +87,7 @@ with tab2:
         "If you want to select your Whisper model / run longer videos pull and run the app locally. \n\n"
         "You do *not* need a GPU to run this locally.  Larger models take more time to process locally, but its doable. \n"
     )
-
+    
 with tab1:
     with st.container(border=True):
         upload_url = st.text_input(label="YouTube / Shorts url", value=st.session_state.upload_url)
@@ -96,13 +111,13 @@ with tab1:
                 label="Whisper transcript",
             )
 
-            model_selection = st.selectbox(
+            st.session_state.model_selection = st.selectbox(
                 label="whisper model (base only in HF space)",
-                index=0,
+                index=st.session_state.model_selection_index,
                 options=avaliable_models,
             )
-            
-            trans_button_val = st.button(label="transcribe with whisper", type="secondary")
+
+            trans_button_val = st.button(label="transcribe with whisper", type="secondary", on_click=transcribe_logic, args=(st.session_state.temporary_video_location, st.session_state.model_selection))
 
     with st.container(border=True):
         input_phrase = st.text_input(
@@ -117,86 +132,5 @@ with tab1:
     colo1, colo2 = st.columns([3, 3])
     col_empty_1, col_orig_video, col_empty_2 = st.columns([4, 8, 4])
 
-    def button_logic(
-        temporary_video_location: str,
-        model_selection: str,
-        input_phrase: list,
-        upload_url: str,
-    ):
-        if trans_button_val:
-            download_video(st.session_state.upload_url, st.session_state.temporary_video_location)
-            filename = open(st.session_state.temporary_video_location, "rb")
-            byte_file = io.BytesIO(filename.read())
-            with open(temporary_video_location, "wb") as out:
-                out.write(byte_file.read())
-                with col_orig_video:
-                    with st.container(border=True):
-                        st.caption("original video")
-                        st.video(temporary_video_location)
-                    out.close()
 
-            st.session_state.whisper_just_transcript, st.session_state.whisper_transcript_words = transcribe(video_file_path=temporary_video_location, model=model_selection)
 
-            with col0.container(border=True):
-                st.text_area(
-                    value=st.session_state.whisper_just_transcript.strip(),
-                    placeholder="transcribe text will be shown here",
-                    label="transcribe text",
-                )
-
-        if clip_button_val:
-            transcript, timestamped_words = transcribe(video_file_path=temporary_video_location, model=model_selection)
-            closest_time_ranges = get_nearest_snippets(input_phrase, transcript, timestamped_words)
-
-            output_clip_path = temporary_video_location[:-4] + "_clip_1.mp4"
-            output_gif_path = temporary_video_location[:-4] + "_clip_1.gif"
-            start_ms = closest_time_ranges[0][0]
-            end_ms = closest_time_ranges[0][1]
-            clip_video_and_gif(temporary_video_location, output_clip_path, output_gif_path, start_ms, end_ms)
-
-            with col0.container(border=True):
-                st.text_area(
-                    value=transcript.strip(),
-                    placeholder="transcribe text will be shown here",
-                    label="transcribe text",
-                )
-
-            c00, c01, c02 = st.columns([8, 8, 8])
-            filename = open(output_clip_path, "rb")
-            byte_file = io.BytesIO(filename.read())
-            with open(output_clip_path, "wb") as out:
-                out.write(byte_file.read())
-                with c00:
-                    with st.container(border=True):
-                        st.caption("clip 1 video")
-                        st.video(output_clip_path)
-                    out.close()
-
-            filename = open(output_gif_path, "rb")
-            byte_file = io.BytesIO(filename.read())
-            with open(temporary_video_location, "wb") as out:
-                out.write(byte_file.read())
-                with c01:
-                    with st.container(border=True):
-                        st.caption("clip 1 gif")
-                        st.image(output_gif_path)
-                    out.close()
-
-                    with open(output_gif_path, "rb") as file:
-                        st.download_button(label="Download gif", data=file, file_name="clip_1.gif", mime="image/gif")
-
-                        # file_ = open(output_gif_path, "rb")
-                        # contents = file_.read()
-                        # data_url = base64.b64encode(contents).decode("utf-8")
-                        # file_.close()
-                        # st.markdown(
-                        #     f'<img src="data:image/gif;base64,{data_url}" alt="clip 1 gif">',
-                        #     unsafe_allow_html=True,
-                        # )
-
-                    out.close()
-
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        st.session_state.temporary_video_location = tmpdirname + "/original_" + str(uuid.uuid4()) + ".mp4"
-
-        # button_logic(temporary_video_location, model_selection, input_phrase, upload_url)
