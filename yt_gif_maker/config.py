390917738ac87bf175adeb3fd980bd4e4b634864
Jeremy Watt <jermwatt@gmail.com>
2024-07-28 16:23:20 -0700

    three clips added to advanced tab

--- yt_gif_maker/config.py
+++ yt_gif_maker/config.py
@@ -7,8 +7,8 @@ default_yt_transcript_words = None
 default_yt_just_transcript = None
 default_whisper_transcript_words = None
 default_whisper_just_transcript = None
-default_upload_url = "https://www.youtube.com/watch?v=svX9-fYMIQU"
-default_input_phrase = "bitches leave"
+default_upload_url = "https://www.youtube.com/watch?v=9m_12SGXNKw"
+default_input_phrase = "say hello to my little friend"
 default_whisper_model_selection = "base"
 default_whisper_model_selection_index = 1
 default_clip_video_path = "./data/input/blank.mp4"
@@ -16,8 +16,8 @@ default_clip_gif_path = "./data/input/blank.jpg"
 default_recovered_phrase = ""
 default_gif_size = ""
 default_text_on_gif_val = True
-default_before_phrase_secs = 2
-default_after_phrase_secs = 3
+default_before_phrase_secs = 0
+default_after_phrase_secs = 2
 default_resize_factor = 0.2
 default_fps = int(15)
 
