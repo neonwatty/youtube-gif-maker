53d93c758e0744976213ba561e860aa7afca9ca0
Jeremy Watt <jermwatt@gmail.com>
2024-07-28 12:31:31 -0700

    three clips added to advanced tab

--- yt_gif_maker/app.py
+++ yt_gif_maker/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from yt_gif_maker.transcribe import avaliable_models
 from yt_gif_maker.streamlit_funcs.state import init_state
-from yt_gif_maker.streamlit_funcs.callbacks import fetch_logic, clip_temp_videos, transcribe_logic
+from yt_gif_maker.streamlit_funcs.callbacks import fetch_logic, clip_and_gif, transcribe_logic, auto_usage
 
 
 app_name = "YouTube gif maker"
@@ -11,9 +11,9 @@ st.title(app_name)
 init_state()
 
 
-tab1, tab2 = st.tabs(["Advanced", "💡 About"])
+tab1, tab2, tab3 = st.tabs(["YouTube gif maker", "Advanced", "💡 About"])
 
-with tab2:
+with tab3:
     st.markdown(
         "### Make a gif of a one liner from any a YouTube video.  \n"
         "How it works: \n\n"
@@ -28,6 +28,102 @@ with tab2:
     )
 
 with tab1:
+    with st.container(border=True):
+        with st.container(border=True):
+            st.markdown("##### Enter url / choose transcriber")
+            cola, colb, colc = st.columns([5, 2, 2])
+            with cola:
+                upload_url = st.text_input(label="YouTube / Shorts url", value=st.session_state.upload_url, key="basic_upload", label_visibility="collapsed")
+            with st.container(border=True):
+                with colb:
+                    use_whisper = st.checkbox(label="use whisper", value=False)
+                with colc:
+                    st.session_state.model_selection = st.selectbox(
+                        label="whisper model",
+                        index=st.session_state.model_selection_index,
+                        options=avaliable_models,
+                        key="auto_model",
+                        disabled=False if use_whisper else True,
+                        label_visibility="collapsed"
+                    )
+        with st.container(border=True):
+            st.markdown("#### Phrase to gif")
+            clip_input_col, clip_button_col, clip_button_check = st.columns([2, 1, 2])
+            with clip_input_col:
+                st.session_state.input_phrase = st.text_input(
+                    label="input phrase",
+                    placeholder="enter in the input phrase you'd like gif-a-fied",
+                    value=st.session_state.input_phrase,
+                    max_chars=34,
+                    label_visibility="collapsed",
+                )
+            with st.expander(label="gif size options"):
+                with st.container(border=True):
+                    (
+                        clip_button_time_before_buffer,
+                        clip_button_time_after_buffer,
+                        clip_button_resize_factor,
+                        clip_button_fps,
+                    ) = st.columns([1, 1, 1, 1])
+                with clip_button_check:
+                    st.session_state.text_on_gif_val = st.checkbox("show input phrase on gif", value=st.session_state.text_on_gif_val)
+                with clip_button_time_before_buffer:
+                    before_phrase_secs = st.number_input("include before (secs)", value=st.session_state.before_phrase_secs, min_value=0, max_value=5)
+                with clip_button_time_after_buffer:
+                    after_phrase_secs = st.number_input("include after (secs)", value=st.session_state.after_phrase_secs, min_value=0, max_value=5)
+                with clip_button_resize_factor:
+                    resize_factor = st.number_input("gif resize factor", value=st.session_state.resize_factor, min_value=0.1, max_value=1.0)
+                with clip_button_fps:
+                    fps = st.number_input("gif fps", value=st.session_state.fps, min_value=10, max_value=60)
+
+                with clip_button_col:
+                    clip_button_val = st.button(
+                        label="phrase-clip",
+                        type="secondary",
+                        on_click=clip_and_gif,
+                        args=(before_phrase_secs, after_phrase_secs, resize_factor, fps, 1),
+                    )
+                
+            
+            
+        #     yt_auto_button = st.button(
+        #         label="fetch video",
+        #         type="secondary",
+        #         on_click=auto_usage,
+        #         args=(upload_url, before_phrase_secs, after_phrase_secs, resize_factor, fps ),
+        #     )
+
+        #     trans_button_val = st.button(
+        #         label="transcribe with whisper",
+        #         type="secondary",
+        #         on_click=transcribe_logic,
+        #         args=(st.session_state.temporary_video_location, st.session_state.model_selection),
+        #     )
+
+        # with st.container(border=True):
+        #     col_clip_1, col_gif_1 = st.columns([4, 4])
+
+        #     with st.container(border=True):
+        #         with col_clip_1:
+        #             with st.container(border=True):
+        #                 st.markdown("#### video from clip 1")
+        #                 st.video(st.session_state.clip_video_paths[0])
+        #             st.text_input(label="similar phrase", value=st.session_state.recovered_phrases[0], key=0)
+
+        #         with col_gif_1:
+        #             with st.container(border=True):
+        #                 st.markdown("#### gif from clip 1")
+        #                 st.image(st.session_state.clip_gif_paths[0])
+        #             st.text_input(label="gif size (in MBs)", value=st.session_state.gif_sizes[0], key=10)
+
+        #             with open(st.session_state.clip_gif_paths[0], "rb") as file:
+        #                 btn = st.download_button(
+        #                     label="download gif", data=file, file_name=f"{st.session_state.input_phrase}.gif", mime="image/gif", key=20
+        #                 )
+
+
+### advanced tab ###
+with tab2:
     with st.container(border=True):
         with st.container(border=True):
             st.markdown("#### upload area")
@@ -111,8 +207,8 @@ with tab1:
             clip_button_val = st.button(
                 label="phrase-clip",
                 type="secondary",
-                on_click=clip_temp_videos,
-                args=(st.session_state.temporary_video_location, before_phrase_secs, after_phrase_secs, resize_factor, fps),
+                on_click=clip_and_gif,
+                args=(before_phrase_secs, after_phrase_secs, resize_factor, fps, 3),
             )
 
         col_clip_1, col_gif_1 = st.columns([4, 4])
--- yt_gif_maker/streamlit_funcs/callbacks.py
+++ yt_gif_maker/streamlit_funcs/callbacks.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import io
 import os
 from yt_gif_maker.transcribe import transcribe
 from yt_gif_maker.yt_download import download_video
@@ -11,7 +10,12 @@ from yt_gif_maker.streamlit_funcs.state import reset_state
 from yt_gif_maker.config import default_temp_video_location
 
 
-def clip_temp_videos(temporary_video_path: str, before_phrase_secs: float, after_phrase_secs: float, resize_factor: float, fps: int) -> None:
+def clip_and_gif(before_phrase_secs: float, after_phrase_secs: float, resize_factor: float, fps: int, num_clips: int = 1) -> None:
+    st.session_state.before_phrase_secs = before_phrase_secs
+    st.session_state.after_phrase_secs = after_phrase_secs
+    st.session_state.resize_factor = resize_factor
+    st.session_state.fps = int(fps)
+
     transcript = st.session_state.yt_just_transcript
     timestamped_words = st.session_state.yt_transcript_words
     if timestamped_words is None:
@@ -20,14 +24,11 @@ def clip_temp_videos(temporary_video_path: str, before_phrase_secs: float, after
     if st.session_state.whisper_transcript_words is not None:
         transcript = st.session_state.whisper_just_transcript
         timestamped_words = st.session_state.whisper_transcript_words
-    st.session_state.before_phrase_secs = before_phrase_secs
-    st.session_state.after_phrase_secs = after_phrase_secs
-    st.session_state.resize_factor = resize_factor
-    st.session_state.fps = int(fps)
-    closest_time_ranges, closest_chunks = get_nearest_snippets(st.session_state.input_phrase, transcript, timestamped_words)
+        
+    for i in range(num_clips):
+        closest_time_ranges, closest_chunks = get_nearest_snippets(st.session_state.input_phrase, transcript, timestamped_words)
 
-    for i in range(3):
-        clip_video_path = "/".join(temporary_video_path.split("/")[:-2]) + f"/test_clip_{str(i+1)}.mp4"
+        clip_video_path = "/".join(st.session_state.temporary_video_path.split("/")[:-2]) + f"/test_clip_{str(i+1)}.mp4"
         clip_file_path_components = clip_video_path.split("/")
         clip_gif_path = "/".join(clip_file_path_components[:-2]) + "/" + clip_file_path_components[-1].split(".")[0] + ".gif"
 
@@ -39,7 +40,7 @@ def clip_temp_videos(temporary_video_path: str, before_phrase_secs: float, after
         st.session_state.clip_video_paths[i] = clip_video_path
         st.session_state.clip_gif_paths[i] = clip_gif_path
 
-        clip_video(temporary_video_path, clip_video_path, start_ms, end_ms, st.session_state.before_phrase_secs, st.session_state.after_phrase_secs)
+        clip_video(st.session_state.temporary_video_path, clip_video_path, start_ms, end_ms, st.session_state.before_phrase_secs, st.session_state.after_phrase_secs)
         make_gif(
             clip_video_path,
             clip_gif_path,
@@ -76,3 +77,8 @@ def transcribe_logic(
     st.session_state.whisper_just_transcript, st.session_state.whisper_transcript_words = transcribe(
         video_file_path=temporary_video_location, model=model_selection
     )
+
+
+def auto_usage(upload_url: str, before_phrase_secs: float, after_phrase_secs: float, resize_factor: float, fps: int) -> None:
+    fetch_logic(upload_url)
+    clip_and_gif(before_phrase_secs, after_phrase_secs, resize_factor, fps, 1)
